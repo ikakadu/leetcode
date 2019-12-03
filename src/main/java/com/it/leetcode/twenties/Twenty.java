4d8994a63f7ccf1f956fe8c42c49424f94880559
wangruitao <wangruitao@lakala.com>
2019-12-03 17:54:40 +0800

    122

--- src/main/java/com/it/leetcode/twenties/Twenty.java
+++ src/main/java/com/it/leetcode/twenties/Twenty.java
@@ -13,13 +13,13 @@ import java.util.List;
 public class Twenty {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0,1,2,2,3,0,4,2};
-//        int[] nums = new int[]{3,2,2,3};
-        int res = removeElement(nums, 2);
-        System.out.println(res);
-        for (int i = 0; i < res; i++) {
-            System.out.print(nums[i] + " ");
-        }
+//        int[] nums = new int[]{0,1,2,2,3,0,4,2};
+////        int[] nums = new int[]{3,2,2,3};
+//        int res = removeElement(nums, 2);
+//        System.out.println(res);
+//        for (int i = 0; i < res; i++) {
+//            System.out.print(nums[i] + " ");
+//        }
 //        List<Integer> list = new ArrayList<>();
 //        list.add(0);
 //        list.add(1);
@@ -31,7 +31,9 @@ public class Twenty {
 //        list.add(2);
 //        List<Integer> integers = removeElement1(list, 2);
 //        System.out.println(integers);
-
+        int[] nums = new int[]{1,2,3,4,5};
+//        System.out.println(maxProfit(nums));
+        System.out.println(maxProfit1(nums));
     }
     //第27题   双指针法
     public static int removeElement(int[] nums, int val) {
@@ -66,6 +68,49 @@ public class Twenty {
             }
         }
        return list;
+    }
+
+    //122 买卖股票的最佳时机 II
+    public static int maxProfit(int[] prices){
+        int flag = 0;//判断当前是否持有
+        int profit = 0;
+        int buyPrice = 0;
+        int sellPrice = 0;
+        if (prices.length<2) return 0;
+        else if(prices.length==2){
+            return prices[0]<prices[1]?prices[1]-prices[0]:0;
+        }else {
+            for (int i = 0; i < prices.length-1; i++) {
+                if(prices[i]<prices[i+1]&&flag==0){
+                    buyPrice = prices[i];
+                    flag =1;
+                }
+                if(flag ==1&&i+3 <= prices.length&&prices[i]<prices[i+1]&&prices[i+1]>prices[i+2]){
+                    sellPrice = prices[i+1];
+                    profit = profit + (sellPrice - buyPrice);
+                    flag =0;
+                }
+                if(flag ==1&&i+2 == prices.length&&prices[i]<prices[i+1]){
+                    sellPrice = prices[i+1];
+                    profit = profit + (sellPrice - buyPrice);
+                    flag =0;
+                }
+
+            }
+            return profit;
+        }
+
+
+    }
+    //122 买卖股票的最佳时机 II   推荐算法 将所有的上升段加在一起，就是最大利润
+    public static int maxProfit1(int[] prices){
+        int profit =0;
+        for (int i = 0; i < prices.length-1; i++) {
+            if (prices[i+1] > prices[i]) {
+                profit += (prices[i+1] - prices[i]);
+            }
+        }
+        return profit;
     }
 
 }
