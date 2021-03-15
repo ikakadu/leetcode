07fafca5d2ff1d2a60e0bce13605b46f04f4893a
wrt <abwrt@163.com>
2021-03-15 21:29:51 +0800

    递归

---
+++ src/main/java/com/it/leetcode/explore/Common.java
@@ -0,0 +1,12 @@
+package com.it.leetcode.explore;
+
+public class Common {
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
--- src/main/java/com/it/leetcode/explore/PrimaryArray.java
+++ src/main/java/com/it/leetcode/explore/PrimaryArray.java
@@ -69,8 +69,17 @@ public class PrimaryArray {
 
 //        int res = romanToInt("MIX");
 //        String res = longestCommonPrefix(new String[]{"flower", "flow", "flight"});
-        String res = intToRoman(58);
-        System.out.println(res);
+//        String res = intToRoman(58);
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        ListNode res = mergeTwoLists(l1, l2);
+
+//        System.out.println(res);
     }
     //第27题   双指针法
     public static int removeElement(int[] nums, int val) {
@@ -688,5 +697,21 @@ public class PrimaryArray {
         return s;
     }
 
+
+
+    //21. 合并两个有序链表
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1==null) return l2;
+        if (l2==null) return l1;
+
+        if (l1.val<l2.val){
+            l1.next  = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l2.next,l1);
+            return l2;
+        }
+
+    }
 
 }
